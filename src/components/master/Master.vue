33e10541b972784f1a40f5eceea55e24ec85f931
Lucas Löffel <lucas@loeffel.io>
2020-04-05 20:24:34 +0200

    add master template

---
+++ src/components/master/Master.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div id="navigation">navigation</div>
+        <slot name="page"></slot>
+        <slot name="outside"></slot>
+        <div id="footer">footer</div>
+    </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component
+export default class Master extends Vue {
+}
+</script>
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,11 +1,20 @@
 <template>
-    <h1>Home</h1>
+    <master-component>
+        <template slot="page">
+            <h1>Home</h1>
+        </template>
+    </master-component>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
+import MasterComponent from '@/components/master/Master.vue';
 
-@Component
+@Component({
+  components: {
+    MasterComponent,
+  },
+})
 export default class Home extends Vue {
 }
 </script>
