6beb8a605a22ed5e02eee4436f0916064668321e
Lucas Löffel <lucas@loeffel.io>
2020-04-26 00:52:53 +0200

    clean up

--- src/Saas.vue
+++ src/Saas.vue
@@ -7,9 +7,7 @@
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
 
-@Component({
-  components: {},
-})
+@Component({})
 export default class Saas extends Vue {
 }
 </script>
