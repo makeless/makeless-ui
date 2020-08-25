c9e7fbdaf995ea08088b847746b34c0e72437baf
Noah Löffel <loeffel.noah@live.de>
2020-08-25 16:33:18 +0200

    release v0.8.4

--- src/models/model.ts
+++ src/models/model.ts
@@ -2,4 +2,5 @@ export default abstract class Model {
   public id: number | null = null;
   public createdAt: Date | null = null;
   public updatedAt: Date | null = null;
+  public deletedAt: Date | null = null;
 }
