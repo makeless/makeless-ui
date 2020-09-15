662b2face31169a3de39356e5c581d5f57fdd171
Noah Löffel <loeffel.noah@live.de>
2020-09-15 10:45:50 +0200

    remove import

--- src/components/selects/team/TeamInvitation.vue
+++ src/components/selects/team/TeamInvitation.vue
@@ -21,7 +21,6 @@
 import {Component, Prop, Vue} from 'vue-property-decorator';
 import VueMultiselect from 'vue-multiselect';
 import 'vue-multiselect/dist/vue-multiselect.min.css';
-import TeamCreate from '../../../structs/team-create';
 import TeamInvitationStruct from '../../../structs/team-invitation';
 
 @Component({
